8d3390843daf8474cbe7b777be7d37c16d9d6cfd
Van PhucPham <Van.PhucPham@ubm.com>
2016-01-04 15:59:58 +0000

    add item entity

--- src/POSBundle/Controller/POSAdminController.php
+++ src/POSBundle/Controller/POSAdminController.php
@@ -11,11 +11,69 @@
     use Symfony\Bundle\FrameworkBundle\Controller\Controller;
     use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
     use Symfony\Component\HttpFoundation\Response;
+    use POSBundle\Entity\Item;
+    use POSBundle\Entity\Categories;
 
 
     class POSAdminController extends Controller
     {
+        function manageItemAction()
+        {
 
+            //$this->deleteCategory();
+            $categories = $this->getCategoryList();
+            $this->deleteCategory($categories);
+            return new Response(
+                '<html><body>Test</body></html>'
+            );
+        }
+
+
+        /**
+         * Get categories
+         *
+         * @return array category
+         */
+        function getCategoryList()
+        {
+            $category = $this->getDoctrine()
+                ->getRepository('POSBundle:Categories')
+                ->findAll();
+            dump($category);
+            if (!$category) {
+                throw $this->createNotFoundException(
+                    'No Cateogry is availabe '
+                );
+            } else {
+                return $category;
+            }
+
+        }
+
+        /**
+         *
+         */
+        function deleteCategory($categories)
+        {
+            $em = $this->getDoctrine()->getManager();
+
+            if (!$categories)
+            {
+                throw $this->createNotFoundException(
+                    'No Cateogry is availabe for deletion'
+                );
+
+            } else {
+                foreach ($categories as $category )
+                {
+                    dump($category->getId());
+//                    $em->remove($category->getId());
+//                    $em->flush();
+                }
+            }
+
+
+        }
 
 
     }
